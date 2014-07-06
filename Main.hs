713380989b4d9cb83301f5463645993458b2063a
Dennis Dietrich <d.dietrich84@gmail.com>
2014-07-06 11:16:35 +0200

    get port from env

--- Main.hs
+++ Main.hs
@@ -1,9 +1,14 @@
 import Network.Wai.Handler.Warp (run)
 import Landing.App (app)
 import System.Environment (getEnv)
+import Control.Exception
+
+getPort :: IO Int
+getPort = (getEnv "PORT" >>= return . read) `catch` (\(SomeException _) -> return 3000)
 
 main :: IO ()
 main = do
-  t <- getEnv "GITHUB_TOKEN"
-  let port = 3000
-  run port app
+  _ <- getEnv "GITHUB_TOKEN"
+  p <- getPort
+  putStrLn $ "Listening on port " ++ show p
+  run p app
