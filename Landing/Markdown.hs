2cff0dc22b0c02975daa1a1cb75fee63ae2f4d30
Dennis Dietrich <d.dietrich84@gmail.com>
2014-07-09 20:58:08 +0200

    update markdown parser

--- Landing/Markdown.hs
+++ Landing/Markdown.hs
@@ -3,17 +3,36 @@
 module Landing.Markdown (parseMarkdown) where
 
 import Text.Pandoc
+import Text.Pandoc.Options
+import Data.Set (Set, fromList)
 import qualified Data.ByteString.Lazy.Char8 as C
 
 parseMarkdown :: C.ByteString -> C.ByteString
-parseMarkdown = C.pack . markdownToHtmlString . C.unpack
+parseMarkdown = C.pack .
+                writeHtmlString writeOptions .
+                readMarkdown readOptions .
+                C.unpack
 
-markdownToHtmlString :: String -> String
-markdownToHtmlString =
-  (writeHtmlString def
-    { writerReferenceLinks = True
-    , writerSectionDivs = True
-    , writerExtensions = githubMarkdownExtensions
-    , writerHighlight = True })
-  . readMarkdown def
-    { readerExtensions = githubMarkdownExtensions }
+writeOptions = def
+  { writerExtensions  = extensions
+  , writerReferenceLinks = True
+  , writerSectionDivs = True
+  , writerHighlight = True }
+
+readOptions = def
+  { readerExtensions = extensions }
+
+extensions :: Set Extension
+extensions = fromList
+  [ Ext_pipe_tables
+  , Ext_raw_html
+  , Ext_tex_math_single_backslash
+  , Ext_fenced_code_blocks
+  , Ext_fenced_code_attributes
+  , Ext_auto_identifiers
+  , Ext_ascii_identifiers
+  , Ext_backtick_code_blocks
+  , Ext_autolink_bare_uris
+  , Ext_intraword_underscores
+  , Ext_strikeout
+  , Ext_lists_without_preceding_blankline ]
